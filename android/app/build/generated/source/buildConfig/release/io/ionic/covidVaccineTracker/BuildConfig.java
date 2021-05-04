72245fa3525fef48464bc0923f03a840b0b5e5d8
Satyam Dua <69812664+satyam-52@users.noreply.github.com>
2021-05-04 16:34:58 +0530

    Add Source Code

---
+++ android/app/build/generated/source/buildConfig/release/io/ionic/covidVaccineTracker/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.ionic.covidVaccineTracker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "io.ionic.covidVaccineTracker";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
